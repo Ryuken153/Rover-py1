45dc653e23ac8522af318074cb56d73d36ada3af
Ryuken153 <yatharthpathak855@gmail.com>
2026-05-02 23:45:36 +0530

    Fix avatar command and improve formatting

--- commands/avatar.py
+++ commands/avatar.py
@@ -1,20 +1,19 @@
 import discord
-from discord.etx import commands
+from discord.ext import commands
 
 class Avatar(commands.Cog):
-  def __init__(self, bot):
-    self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-  @commands.command(aliases=["av"])
-  async def Avatar(self, ctx: commands.Context, member: discord.Member = None):
-      member = member or ctx.author
+    @commands.command(aliases=["av"])
+    async def avatar(self, ctx: commands.Context, member: discord.Member = None):
+        member = member or ctx.author
 
-      embed = discord.Embed(title = f"{member.display_name}'s Avatar", color = member.color)
-      embed.set_image(url=member.display.avatar.url)
-      embed.set_footer(text= f"Requested by {ctx.author.display_name}")
-   
-      await ctx.reply(embed=embed)
+        embed = discord.Embed(title=f"{member.display_name}'s Avatar", color=member.color)
+        embed.set_image(url=member.display_avatar.url)
+        embed.set_footer(text=f"Requested by {ctx.author.display_name}")
+
+        await ctx.reply(embed=embed)
 
 async def setup(bot):
-  await bot.add_cog(Avatar(bot))
-    
+    await bot.add_cog(Avatar(bot))
