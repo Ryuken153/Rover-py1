50c03fd662af2611e3cc60764f4de783835d1da4
Ryuken153 <yatharthpathak855@gmail.com>
2026-05-02 23:00:25 +0530

    Fix AsyncIoMotorClient instantiation

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = motor.motor_asyncio.AsyncIoMotorClient(os.getenv("MONGO_URI"))
+client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv("MONGO_URI"))
 db = client["Rover"]
 
 guilds_col = db["guilds"]
