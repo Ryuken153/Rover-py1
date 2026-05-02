e0744eef05a380f0a9282783b9755c15de732804
Ryuken153 <yatharthpathak855@gmail.com>
2026-05-02 20:51:11 +0530

    Update ping.py

--- commands/ping.py
+++ commands/ping.py
@@ -1,1 +1,15 @@
+from discord.ext import commands 
 
+class ping(commands.Cog):
+  def __init__(self, bot):
+
+    @commands.command()
+    async def ping(self ctx: commands.Context):
+      """Check bot latency."""
+      latenc = round(self.bot.latency * 1000)
+      msg = await crx.reply("Pinging...")
+      roundtrip = round((msg.created_at - ctx.message.created_at).total_seconds() * 1000)
+      await msg.edit(content = f"🏓 Pong! Latency: **{roundtrip}ms** | API: **{latency}ms**")
+
+async def setup(bot):
+  await bot.add_cog(Ping(bot))
