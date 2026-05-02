31e5c6b69828fe3b00d3ea259c7837dde7f8c203
Ryuken153 <yatharthpathak855@gmail.com>
2026-05-03 00:36:58 +0530

    Change command prefix from dynamic to static '!'

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ intents.message_content = True
 intents.members = True
 intents.guilds = True
 
-bot = commands.Bot(command_prefix=get_prefix, intents=intents, help_command=None)
+bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
 
 @bot.event
 async def on_message(message):
