8a7243ca271909f5eb066755a3537c0b85d5646e
Ryuken153 <yatharthpathak855@gmail.com>
2026-05-02 23:35:57 +0530

    Update avatar.py

--- commands/avatar.py
+++ commands/avatar.py
@@ -5,16 +5,16 @@ class Avatar(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
 
-@commands.command(aliases=["av"]+)
-async def Avatar(self, ctx: commands.Context, member: discord.Member = None):
-  member = member or ctx.author
+  @commands.command(aliases=["av"]+)
+  async def Avatar(self, ctx: commands.Context, member: discord.Member = None):
+      member = member or ctx.author
 
-  embed = discord.Embed(title = f"{member.display_name}'s Avatar", color = member.color)
-  embed.set_image(url=member.display.avatar.url)
-  embed.set_footer(text= f"Requested by {ctx.author.display_name}")
-
+      embed = discord.Embed(title = f"{member.display_name}'s Avatar", color = member.color)
+      embed.set_image(url=member.display.avatar.url)
+      embed.set_footer(text= f"Requested by {ctx.author.display_name}")
+   
   await ctx.reply(embed=embed)
 
-async def(bot):
+async def setup(bot):
   await bot.add_cog(Avatar(bot))
     
