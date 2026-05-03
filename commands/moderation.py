57e722f18efdd724f85da9cc4c34b24db311b3cd
Ryuken153 <yatharthpathak855@gmail.com>
2026-05-03 14:13:03 +0530

    Create moderation.py

---
+++ commands/moderation.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+from datetime import timedelta
+from utils import can_action_kick, can_kick, can_ban,  can_mute, can_manage_message
+
+def mod_embed(title: str, description: str, color: deiscord.Color.orange()) -> discord.Embed:
+  return discord.Embed(title=title, description=discription, color=color)
+
+class Moderation(commands.Cog):
+  def __init__(self, bot: commands.bot):
+    self.bot = bot
+
+  @commands.command()
+  @commands.bot_has_permissions(kick_members=True)
+  async def kick(self, ctx, member: discord.Member, *, reason: str = "No reson provided"):
+    if not can_kick(ctx.author):
+      return await ctx.send(embed=mod_embed("No permission", "you need a authority called **Kick members**."))
+    if member = ctx.author:
+      return await ctx.send(embed=mod_embed("Error", "you cannot kick yourself baka ~~~"))
+    if not can_action_kick(ctx.author, member):
+      return await ctx.send(embed=mod_embed("Error", "you cannot kick someone equal oe higher authority")
+
+    await member.kick(reason=reason)
+    await ctx.send(embed=mod_embed(
+      "Member kicked",
+      f"**{member}** has been kicked.\n **Reason:** {reason}",
+      discord.Color.orange()
+    ))
+  
